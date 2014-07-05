51253ac33d47839588a2a0c002ee7a3280207a3b
Will Jun <hejun.run@gmail.com>
2014-07-05 18:06:38 +0800

    fetcher

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+
 var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname);
 
 app.get('/', function(req, res){
-  app.set('view engine', 'jade');
-  app.set('views', __dirname);
   res.render('index');
+});
+
+app.get('/fetch', function(req, res){
+  request(
+    req.query.url,
+    function(error, result, body){
+      var $ = cheerio.load(body);
+
+      //TODO
+      var Parser = require('./parser/amazoncn.js');
+      var p = new Parser($);
+      var j = p.getJSON();
+
+      res.set('Content-Type', 'application/json');
+      res.send(j);
+    }
+  );
 });
 
 app.listen(3001, function(){
