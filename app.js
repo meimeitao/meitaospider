f12bb6896e8015cc83d8b26478e67f8c4340c3c3
Phil <philying@gmail.com>
2014-07-09 23:32:28 +0800

    add taobao.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
+var iconv = require('iconv-lite');
+var urls = require('url');
 
 var app = express();
 app.set('view engine', 'jade');
@@ -11,18 +13,35 @@ app.get('/', function(req, res){
 });
 
 app.get('/fetch', function(req, res){
+  var url = req.query.url
+  var parser = '';
   request(
-    req.query.url,
+    url,
     function(error, result, body){
+      console.log(urls.parse(url).hostname);
+      switch(urls.parse(url).hostname){
+        case "item.taobao.com":
+          parser = './parser/taobao.js';
+          body = iconv.decode(body, 'gbk');
+          break;
+        case "amazon.cn":
+          parser = './parser/amazoncn.js';
+          break;
+        default:
+          console.log("parser not found "+url);
+          break;
+      }
       var $ = cheerio.load(body);
 
       //TODO
-      var Parser = require('./parser/amazoncn.js');
-      var p = new Parser($);
-      var j = p.getJSON();
+      if(parser != ""){
+        var Parser = require(parser);
+        var p = new Parser($);
+        var j = p.getJSON();
 
-      res.set('Content-Type', 'application/json');
-      res.send(j);
+        res.set('Content-Type', 'application/json');
+        res.send(j);
+      }
     }
   );
 });
