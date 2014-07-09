976cb3ea5dbd2a69b15238b9aab36fb0e4d12fe1
Phil <philying@gmail.com>
2014-07-09 23:32:44 +0800

    add taobao.js

---
+++ parser/taobao.js
@@ -0,0 +1,15 @@
+module.exports = function($) {
+  this.$ = $;
+  this.getJSON = function() {
+  	var obj = {};
+  	obj.title = this.$("title").text();
+  	obj.price = this.$("#J_StrPrice .tb-rmb-num").text();
+    var imgs = Array();
+    this.$("#J_UlThumb").find("img").each(function(i, elem){
+      imgs.push($(this).attr("data-src").replace(/_50x50.jpg/,''));
+      obj.img = imgs;
+    });
+    obj.img = imgs;
+  	return JSON.stringify(obj);
+  };
+}
