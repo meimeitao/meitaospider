5d392243ed350b3a7b3a8ba36b7f8402d9280c9d
Phil <philying@gmail.com>
2014-07-10 23:16:20 +0800

    fix image fetch logic

--- parser/jd.js
+++ parser/jd.js
@@ -4,6 +4,15 @@ module.exports = function($) {
   	var obj = {};
   	obj.title = this.$("title").text();
     obj.price = this.$("#summary-price #jd-price").text();
+    var imgs = Array();
+    this.$("#spec-list .spec-items").find("img").each(function(i, elem){
+      imgs.push($(this).attr("src").replace('n5','n1'));
+    });
+    obj.img = imgs;
+    this.$("body").find("link").each(function(i, elem){
+      obj.url = $(this).attr("href");
+    });
+    obj.currency = 'RMB';
   	return JSON.stringify(obj);
   };
 }
