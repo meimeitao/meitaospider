ad8210338d42d496429851fc0cdfead8d013acc3
raydya <dya.ilaempire@gmail.com>
2015-04-08 15:45:56 +0800

    phantom js timeout

--- phantom.js
+++ phantom.js
@@ -6,6 +6,16 @@ if(args.length < 2){
   phantom.exit();
 }
 var url = args[1];
+
+page.settings.resourceTimeout = 45000;
+page.onResourceTimeout = function(e) {
+  console.log(page.content);
+  phantom.exit(1);
+};
+//page.onNavigationRequested = function(url, type, willNavigate, main) {
+//  console.log(page.content);
+//  phantom.exit();
+//};
 page.open(url, function (status) {
   //Page is loaded!
   console.log(page.content);
--- phantompost.js
+++ phantompost.js
@@ -7,6 +7,11 @@ if(args.length < 2){
 }
 var url = args[1];
 var postBody = "mytrakpakid=GB&mytrakpaknumber="+args[2];
+page.settings.resourceTimeout = 45000;
+page.onResourceTimeout = function(e) {
+  console.log(page.content);
+  phantom.exit(1);
+};
 page.open(url, 'POST', postBody, function (status) {
   //Page is loaded!
   console.log(page.content);
