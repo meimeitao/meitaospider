972768d8a77f67940775cea8d0f98b2dab6abfc8
raydya <dya.ilaempire@gmail.com>
2015-07-28 17:51:10 +0800

    updated

--- parser/mashort.js
+++ parser/mashort.js
@@ -2,7 +2,7 @@ module.exports = function($) {
   this.$ = $;
   this.getJSON = function() {
   	var obj = {};
-    obj.redirectUrl = this.$("#J_Redirect").val();
+    obj.redirectUrl = this.$("#J_Url").val();
   	return JSON.stringify(obj);
   };
 };
