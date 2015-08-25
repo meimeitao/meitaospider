b2f8d0b070a39c5571e6b0654002393287a42fce
raydya <dya.ilaempire@gmail.com>
2015-08-25 12:02:48 +0800

    ralphlauren

--- pageAutomation/ralphlauren.js
+++ pageAutomation/ralphlauren.js
@@ -10,48 +10,87 @@ var system = require('system');
 var args = casper.cli.args;
 
 var url = args[0];
-var stockMapping = JSON.parse(args[1]);
 
-//casper.on("remote.message", function(message) {
-//  this.echo("remote console.log: " + message);
-//});
-//
-//casper.on('page.error', function (msg, trace) {
-//    this.echo( 'Error: ' + msg, 'ERROR' );
-//});
+function parseMoney(amount) {
+  return Number(amount.replace(/[^0-9\.]+/g,""));
+}
+
+var retData = {};
 
 casper.start(url);
 
 casper.then(function() {
-  var tmpStock, stockValue, tmpTarget, mapping = [];
-  for (var x in stockMapping) {
-    tmpStock = stockMapping[x];
-    for (var m in tmpStock) {
-      if (m == 'soldout') continue;
-      tmpTarget = tmpStock[m];
-      this.evaluate(function setProperties(id, value) {
-        var evt = document.createEvent('CustomEvent');
-        evt.initCustomEvent('click', true, false);
-        var ele = document.querySelector("#"+id+" li[title='"+value+"']");
-        ele.dispatchEvent(evt);
-      }, m, tmpTarget);
-    }
-    //this.capture('runtime/screenshot_'+x+'.png');
-    stockValue = this.evaluate(function getStockStatus() {
-      return document.querySelector("#addToCart").getAttribute("title");
-    });
-    if (stockValue == 'Out Of Stock') {
-      stockMapping[x].soldout = 1;
-      mapping.push(stockMapping[x]);
-    } else {
-      stockMapping[x].soldout = 0;
-      mapping.push(stockMapping[x]);
-    }
+  var properties = [], stocks = [];
+  var primitivePriceCurrency = "USD";
+  var colorID = "color";
+  var colorName = "color";
+  var sizeID = "size";
+  var sizeName = "size";
+
+  var itemMap = this.evaluate(function() {
+    return itemMap;
+  });
+
+  var orderedImageMap = this.evaluate(function() {
+    return orderedImageMap_0;
+  });
+
+  var imageMap = {};
+  for (var x in orderedImageMap) {
+    imageMap[orderedImageMap[x]['cId']] = orderedImageMap[x];
   }
-  utils.dump(mapping);
+
+  var color = {}, size = {};
+  color['name'] = colorName;
+  color['id'] = colorID;
+  color['data'] = {};
+  size['name'] = sizeName;
+  size['id'] = sizeID;
+  size['data'] = {};
+
+  for (var x in itemMap) {
+    var tmpItem = itemMap[x];
+    var tmpImage = imageMap[tmpItem["cId"]];
+
+    var tmpColorObject = {
+      desc: tmpItem["cDesc"]
+      , demo: tmpImage ? tmpImage["v400"] : ""
+      , sample: tmpImage ? tmpImage["swatch"] : ""
+      , primitive_price: parseMoney(tmpItem["price"])
+      , primitive_price_currency: primitivePriceCurrency
+      , exID: tmpItem["cId"]
+    };
+
+    color["data"][tmpItem["cId"]] = tmpColorObject;
+
+    var tmpSizeObject = {
+      desc: tmpItem["sDesc"]
+      , demo: ""
+      , sample: ""
+      , primitive_price: 0
+      , primitive_price_currency: primitivePriceCurrency
+      , exID: tmpItem["sId"]
+    };
+
+    size["data"][tmpItem["sId"]] = tmpSizeObject;
+    
+    tmpStockObj = {};
+    tmpStockObj[colorID] = tmpItem["cId"];
+    tmpStockObj[sizeID] = tmpItem["sId"];
+    tmpStockObj["soldout"] = tmpItem["quantityOnHand"] > 0 ? 0 : 1;
+    stocks.push(tmpStockObj);
+  }
+
+  properties.push(color);
+  properties.push(size);
+
+  retData = {
+    "properties": properties
+    , "stocks": stocks
+  };
 });
 
 casper.run(function() {
-    //this.debugPage();
-    this.exit();
+  utils.dump(retData);
+  this.exit();
 });
