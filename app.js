fb3ca6017bc66b999d8a3e9439451378e17e6719
raydya <dya.ilaempire@gmail.com>
2015-12-10 12:19:11 +0800

    updated

--- app.js
+++ app.js
@@ -282,6 +282,9 @@ app.post('/salesProperties', urlencodedParser, function(req, res) {
     case "www.lastcall.com":
       parser = './pageAutomation/lastcall.js';
       break;
+    case "www.allsole.com":
+      parser = './pageAutomation/allsole.js';
+      break;
     //case "www.myhabit.com":
     //  parser = './pageAutomation/myhabit.js';
     //  break;
