14fddd3eec47b1bdf1d3f1de192d10e8027c8333
leturtle <turtle_ct@hotmail.com>
2016-05-23 15:49:27 +0800

    fix typo

--- bc/list/mp.weixin.qq.com.js
+++ bc/list/mp.weixin.qq.com.js
@@ -1,6 +1,6 @@
 module.exports = function($, url) {
   var urls = require('url');
-  this.getJSON = funciton() {
+  this.getJSON = function() {
     var obj = {};
     obj.urls = $('.weui_msg_card .weui_media_title').map(function(){
       return urls.resolve(url, $(this).attr('hrefs'));
